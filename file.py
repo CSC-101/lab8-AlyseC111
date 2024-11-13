eb6738a22dc1dff84f5127b00a8244f93dce91c4
AlyseC111 <alcourtn@calpoly.edu>
2024-11-12 22:14:24 -0800

    Lab 8 Submission

--- file.py
+++ file.py
@@ -1,23 +1,26 @@
 import sys
-
+#our program should open the file and read each line until the entire contents have been read.
+# Each line in the file is expected to consist of two float values (how can you split a line into
+# these two separate values?). For each line, convert the float values and print their sum.
 if __name__ == '__main__':
     print(sys.argv)
 
     try:
-       with open('sys.argv', 'r') as infile:
+       with open(sys.argv[1], 'r') as infile:
            content = infile.readlines()
            for i in range(len(content)):
+               content[i] = content[i].rstrip('\n')
                elem = content[i].split(' ')
-               sum = 0
-               if len(elem) == 2 and elem[0] == float and elem[1] == float:
-                   for j in elem:
-                       sum += j
+               if len(elem) != 2:
+                   print("Error - line did not contain 2 elements")
                else:
-                   print("Error; line cannot be processed")
-               print(sum)
+                    try:
+                        elem[0] = float(elem[0])
+                        elem[1] = float(elem[1])
+                        print(elem[0] + elem[1])
+                    except ValueError:
+                        print("Error - line cannot be processed")
     except IOError as e:
         print("Cannot open file: ", e)
-    except ValueError as e:
-        print("No File Provided: ", e)
 
 
